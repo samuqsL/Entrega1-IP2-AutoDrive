d94f298a1d4ad280d702422bbf0a4723b6be3ef6
samuqsL <samuelcely123@gmail.com>
2026-04-17 20:37:55 -0300

    Add Cliente class with constructors and fields

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,28 @@
+package model
+
 Public class Cliente {
-  //Construtor
-  //getter
-  //setter etc...
+
+  private String nome;
+  private String cpf;
+  private String cnh;
+  private String email;
+  private String telefone;
+
+  //Construtor 1 (Principal)
+  public Cliente (String nome, String cpf, String cnh, String email, String telefone){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.cnh = cnh;
+    this.email = email;
+    this.telefone = telefone;
+  }
+  //Construtor 2 (overloading[simplificando] --> direciona pro construtor principal)
+   public Cliente (String nome, String cpf, String cnh){
+     this(nome, cpf, cnh, null, null);
+   }
+
+  //getters
+  public void getNome() {return this.nome}
+  pu
+  
 }

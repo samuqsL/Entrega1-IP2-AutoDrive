6fc6eba34a59854b702ff0b78b1bec62fd5630fb
Artur Cândido <arturcdn10@gmail.com>
2026-04-17 19:34:36 -0300

    Atualização VeiculoNovo.java
    implementado o primeiro codigo de VeiculoSeminovo

--- src/model/VeiculoNovo.java
+++ src/model/VeiculoNovo.java
@@ -1,1 +1,12 @@
+package projeto1;
 
+//classe filha de "Veiculos"
+public class VeiculoNovo extends Veiculo {
+
+	//adicionar atributos [?]
+	
+	//construtor
+public VeiculoNovo(String chassi, String renavam, String modelo, int ano) {
+		super(chassi, renavam, modelo, ano);
+	}
+}

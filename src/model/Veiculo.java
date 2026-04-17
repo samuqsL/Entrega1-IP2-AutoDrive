da48ad1eeda5a30c4cc763009ebab6c3ad8515e7
Artur Cândido <arturcdn10@gmail.com>
2026-04-17 19:28:31 -0300

    Atualização Veiculo.java
    implementado o primeiro codigo da classe Veiculo.

--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -1,3 +1,76 @@
+//sinais de comunicação do meu codigo:
+// [!] importante
+// [!!] muito importante
+// [!!!] urgente
+// [?] duvida
+// [A] aviso
+
+package projeto1;
+
+//classe mae de "VeiculoNovo" e "VeiculoSeminovo"
 public class Veiculo {
-  
+	//atributos
+	private String chassi;
+	private String renavam;
+	private String modelo;
+	private int ano;
+	private StatusVeiculo status;
+	
+	//construtores
+public Veiculo(String chassi, String renavam, String modelo, int ano, StatusVeiculo status) {
+		this.chassi = chassi;
+		this.renavam = renavam;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.status = status;
+	}
+public Veiculo(String chassi, String renavam, String modelo, int ano) {
+	this.chassi = chassi;
+	this.renavam = renavam;
+	this.modelo = modelo;
+	this.ano = ano;
+	status = StatusVeiculo.ESTOQUE;
+}
+
+    //getters e setters
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+
+	public String getRenavam() {
+		return renavam;
+	}
+
+	public void setRenavam(String renavam) {
+		this.renavam = renavam;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public StatusVeiculo getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusVeiculo status) {
+		this.status = status;
+	}
+
 }

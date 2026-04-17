2f8b24a138abe511beb47b87a0a23acab5376926
Artur Cândido <arturcdn10@gmail.com>
2026-04-17 19:31:56 -0300

    Atualização VeiculoSeminovo.java
    implementado o primeiro codigo da classe VeiculoSeminovo

--- src/model/VeiculoSeminovo.java
+++ src/model/VeiculoSeminovo.java
@@ -1,1 +1,22 @@
+package projeto1;
 
+//classe filha de "Veiculo"
+public class VeiculoSeminovo extends Veiculo {
+	//atributos
+	private double quilometragem;
+  //não permitir cadastro de seminovos sem registro de quilometragem inicial [A] [!!]
+	
+	//construtor
+public VeiculoSeminovo(String chassi, String renavam, String modelo, int ano, double quilometragem) {
+		super(chassi, renavam, modelo, ano);
+		this.quilometragem = quilometragem;
+	}
+
+//getters e setters
+public double getQuilometragem() {
+return quilometragem;
+    }
+public void setQuilometragem(double quilometragem) {
+	this.quilometragem = quilometragem;
+    }
+}

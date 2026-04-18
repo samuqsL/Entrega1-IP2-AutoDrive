fec697ecf40f82a263d4c9dc2c046889aa99825f
samuqsL <samuelcely123@gmail.com>
2026-04-17 21:06:43 -0300

    Add Vendedor class with properties and methods

--- src/model/Vendedor.java
+++ src/model/Vendedor.java
@@ -1,1 +1,20 @@
+public class Vendedor {
 
+  private String nome;
+  private double comissao;
+
+  //construtor principal da classe!
+  public Vendedor(String nome, double comissao){
+    this.nome = nome;
+    this.comissao = comissao;
+  }
+
+  //getters (pegar valor)
+  public void getNome() {return this.nome;}
+  public void getComissao() {return this.comissao;}
+  //setters
+  public String setNome(String nome) {this.nome = nome;}
+  public double setComissao(double comissao) {this.comissao = comissao;}
+
+  //Metodos (...)
+}

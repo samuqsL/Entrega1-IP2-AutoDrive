48464f4892abc61a6e495d3f99b98345a1c70547
samuqsL <samuelcely123@gmail.com>
2026-04-17 21:12:12 -0300

    Fix getter and setter method signatures in Vendedor

--- src/model/Vendedor.java
+++ src/model/Vendedor.java
@@ -12,11 +12,11 @@ public class Vendedor {
   }
 
   //getters (pegar valor)
-  public void getNome() {return this.nome;}
-  public void getComissao() {return this.comissao;}
+  public String getNome() {return this.nome;}
+  public double getComissao() {return this.comissao;}
   //setters
-  public String setNome(String nome) {this.nome = nome;}
-  public double setComissao(double comissao) {this.comissao = comissao;}
+  public void setNome(String nome) {this.nome = nome;}
+  public void setComissao(double comissao) {this.comissao = comissao;}
 
   //Metodos (...)
 }

d8185d0c13d3bcc5eae81f96dfcf4ce1608e5de4
samuqsL <samuelcely123@gmail.com>
2026-04-18 14:01:26 -0300

    Update VeiculoSeminovo.java

--- src/model/VeiculoSeminovo.java
+++ src/model/VeiculoSeminovo.java
@@ -5,8 +5,8 @@ public class VeiculoSeminovo extends Veiculo {
 	//atributos [são herdados da classe Veiculo]!
 	
 	//construtor principal
-	public VeiculoSeminovo(String chassi, String renavam, String modelo, int ano, double preco, StatusVeiculo status, double quilometragem) {
-		super(chassi, renavam, modelo, ano, preco, status.ESTOQUE);
+	public VeiculoSeminovo(String chassi, String renavam, String modelo, int ano, double preco, double quilometragem) {
+		super(chassi, renavam, modelo, ano, preco, quilometragem);
 		if (quilometragem <= 0) {
             throw new IllegalArgumentException("Seminovo precisa de quilometragem inicial");
         }

e1e480ac438b100a739e5252a9d699f5a32ed271
samuqsL <samuelcely123@gmail.com>
2026-04-17 22:32:56 -0300

    Add preco attribute and update constructors in Veiculo

--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -14,44 +14,44 @@ public class Veiculo {
 	private String renavam;
 	private String modelo;
 	private int ano;
+	private double preco;
 	private StatusVeiculo status;
 	
 	//construtores
-public Veiculo(String chassi, String renavam, String modelo, int ano, StatusVeiculo status) {
+	public Veiculo(String chassi, String renavam, String modelo, int ano, StatusVeiculo status) {
 		this.chassi = chassi;
 		this.renavam = renavam;
 		this.modelo = modelo;
 		this.ano = ano;
+		this.preco = preco;
 		this.status = status;
 	}
-public Veiculo(String chassi, String renavam, String modelo, int ano) {
-	this.chassi = chassi;
-	this.renavam = renavam;
-	this.modelo = modelo;
-	this.ano = ano;
-	status = StatusVeiculo.ESTOQUE;
-	//esse construtor não tem "StatusVeiculo status" como parametro, pra deixar o status "ESTOQUE" como status padrão
+	public Veiculo(String chassi, String renavam, String modelo, int ano) {
+		this.chassi = chassi;
+		this.renavam = renavam;
+		this.modelo = modelo;
+		this.ano = ano;
+		status = StatusVeiculo.ESTOQUE;
+		//esse construtor não tem "StatusVeiculo status" como parametro, pra deixar o status "ESTOQUE" como status padrão
 }
 
     //getters e setters
 	public String getChassi() {return chassi;}
-
 	public void setChassi(String chassi) {this.chassi = chassi;}
 
 	public String getRenavam() {return renavam;}
-
 	public void setRenavam(String renavam) {this.renavam = renavam;}
 
 	public String getModelo() {return modelo;}
-
 	public void setModelo(String modelo) {this.modelo = modelo;}
 
 	public int getAno() {return ano;}
-
 	public void setAno(int ano) {this.ano = ano;}
 
-	public StatusVeiculo getStatus() {return status;}
+	public double getPreco() {return preco;}
+	public void setPreco() {this.preco = preco;}
 
+	public StatusVeiculo getStatus() {return status;}
 	public void setStatus(StatusVeiculo status) {this.status = status;}
 
 }

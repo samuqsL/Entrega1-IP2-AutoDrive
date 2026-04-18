6b680ca7218a384eecf55367e16218645b7d586b
samuqsL <samuelcely123@gmail.com>
2026-04-18 13:26:55 -0300

    Update VeiculoNovo constructor to include new attributes

--- src/model/VeiculoNovo.java
+++ src/model/VeiculoNovo.java
@@ -1,11 +1,8 @@
 package model
 //classe filha de "Veiculos"
 public class VeiculoNovo extends Veiculo {
-
-	//adicionar atributos [?]
-	
 	//construtor
-	public VeiculoNovo(String chassi, String renavam, String modelo, int ano) {
-			super(chassi, renavam, modelo, ano);
+	public VeiculoNovo(String chassi, String renavam, String modelo, int ano, double preco, double quilometragem) {
+			super(chassi, renavam, modelo, ano, preco, quilometragem);
 	}
 }

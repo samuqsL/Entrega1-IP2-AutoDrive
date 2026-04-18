7eeb4efa292dd380dc52ab22e5a960351d6049dc
samuqsL <samuelcely123@gmail.com>
2026-04-17 22:12:46 -0300

    Add 'Veiculo' attribute to Venda class
    Added 'Veiculo' attribute and updated constructor and methods.

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -1,10 +1,37 @@
 package model;
 
+import java.util.LocalDateTime;
+  
 public class Venda {
-  //atributos
- private Cliente cliente;
- private Vendedor vendedor;
- private double valorTotal;
- private double entrada;
- private LocalDateTime dataVenda;
+  //Atributos
+  private Cliente cliente;
+  private Vendedor vendedor;
+  private Veiculo veiculo;
+  private double valorTotal;
+  private LocalDateTime dataVenda;
+
+  //Construtor Principal
+  public Venda (Cliente cliente, Vendedor vendedor, Veiculo veiculo, double valorTotal, LocalDateTime dataVenda){
+    this.cliente = cliente;
+    this.vendedor = vendedor;
+    this.veiculo = veiculo;
+    this.valorTotal = valorTotal;
+    this.dataVenda = dataVenda;
+  }
+
+  //getters (pegar valor)
+  public Cliente getCliente() {return this.cliente;}
+  public Vendedor getVendedor() {return this.vendedor;}
+  public Veiculo getVeiculo() {return this.veiculo;}
+  public double getValorTotal() {return this.valorTotal;}
+  public LocalDateTime getDataVenda() {return this.dataVenda;}
+  //setters (alterar valor / validações)
+  public void setCliente(Cliente cliente) {this.cliente = cliente;}
+  public void setVendedor(Vendedor vendedor) {this.vendedor = vendedor;}
+  public void setVeiculo(Veiculo veiculo) {this.veiculo = veiculo;}
+  public void setValorTotal(double valorTotal) {this.valorTotal = valorTotal;}
+  public void setDataVenda(LocalDateTime dataVenda) {this.dataVenda = dataVenda;}
+
+  public 
+  
 }

ce71b4bf0edcf7bd8bec48ad7d6270926a7f8d5a
samuqsL <samuelcely123@gmail.com>
2026-04-17 21:40:20 -0300

    Add attributes to Venda class
    Added attributes for Cliente, Vendedor, valorTotal, entrada, and dataVenda.

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -1,6 +1,10 @@
 package model;
 
 public class Venda {
-
-  
+  //atributos
+ private Cliente cliente;
+ private Vendedor vendedor;
+ private double valorTotal;
+ private double entrada;
+ private LocalDateTime dataVenda;
 }

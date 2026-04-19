b81b34b949782a617cf24f39a9909df44cfe6e37
OTAVIO-SANTIAGO <santiago888f1@gmail.com>
2026-04-19 12:38:42 -0300

    Update Notificacao.java

--- src/model/Notificacao.java
+++ src/model/Notificacao.java
@@ -1,10 +1,16 @@
 package model;
 
 public class Notificacao {
+    private double quilometragem;
+    private int revisaoNumero;
+    private String data;
     private Cliente cliente;
     private Veiculo veiculo;
 
-    public Notificacao(Cliente cliente, Veiculo veiculo) {
+    public Notificacao(double quilometragem, int revisaoNumero, String data, Cliente cliente, Veiculo veiculo) {
+        this.quilometragem = quilometragem;
+        this.revisaoNumero = revisaoNumero;
+        this.data = data;
         this.cliente = cliente;
         this.veiculo = veiculo;
     }
@@ -17,9 +23,15 @@ public class Notificacao {
     }
 
     public void notificarCliente() {
+        if (cliente == null || veiculo == null) {
+        System.out.println("Erro: Falha ao enviar notificação. Dados incompletos.");
+        return;
+        }
+        
         System.out.println("ENVIANDO NOTIFICAÇÃO...");
         System.out.println("Para: " + cliente.getNome());
         System.out.println("Assunto: Status do seu veículo " + veiculo.getModelo());
+        System.out.println("Data: " + this.data + " | KM: " + this.quilometragem);
         System.out.println("Mensagem: O status atual é: " + veiculo.getStatus());
     }
 }

c2a06417c007640d13678077c3886518b44bd50d
samuqsL <samuelcely123@gmail.com>
2026-04-19 11:52:51 -0300

    Add entrada field and its getter/setter in Venda

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -20,6 +20,7 @@ public class Venda {
     this.cliente = cliente;
     this.vendedor = vendedor;
     this.veiculo = veiculo;
+    this.entrada = entrada;
   }
   //Contrutor 2 (Overloading[sem entrada]) - Se "entrada" não for especificada ela inicia com "0"!
   public Venda (Cliente cliente, Vendedor vendedor, Veiculo veiculo){
@@ -32,12 +33,14 @@ public class Venda {
   public Veiculo getVeiculo() {return this.veiculo;}
   public double getValorTotal() {return this.valorTotal;}
   public LocalDateTime getDataVenda() {return this.dataVenda;}
+  public double getEntrada() {return this.entrada;}
   //setters (alterar valor / validações)
   public void setCliente(Cliente cliente) {this.cliente = cliente;}
   public void setVendedor(Vendedor vendedor) {this.vendedor = vendedor;}
   public void setVeiculo(Veiculo veiculo) {this.veiculo = veiculo;}
   public void setValorTotal(double valorTotal) {this.valorTotal = valorTotal;}
   public void setDataVenda(LocalDateTime dataVenda) {this.dataVenda = dataVenda;}
+  public void setEntrada(double entrada) {this.entrada = entrada;}
 
  //Metodo 1 (realizar venda de veiculo) - [PRINCIPAL]
   public void realizarVenda() {

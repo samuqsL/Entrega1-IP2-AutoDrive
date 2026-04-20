2b9196ae5afa74fb09b2b32ff00ada1e54fe988f
samuqsL <samuelcely123@gmail.com>
2026-04-20 16:14:05 -0300

    Update Notificacao.java

--- src/model/Notificacao.java
+++ src/model/Notificacao.java
@@ -2,13 +2,15 @@ package model;
 
 public class Notificacao {
     private double quilometragem;
+    private int revisaoNumero;
     private String data;
     private int mesesUso;
     private Cliente cliente;
     private Veiculo veiculo;
 
-    public Notificacao(double quilometragem, String data, int mesesUso, Cliente cliente, Veiculo veiculo) {
+    public Notificacao(double quilometragem, int revisaoNumero, String data, int mesesUso, Cliente cliente, Veiculo veiculo) {
         this.quilometragem = quilometragem;
+        this.revisaoNumero = revisaoNumero;
         this.data = data;
         this.mesesUso = mesesUso;
         this.cliente = cliente;
@@ -17,8 +19,11 @@ public class Notificacao {
 
     // REQ10: revisão preventiva por tempo/km
     public boolean gerarAlerta() {
-        return quilometragem >= 10000 || mesesUso >= 12;
+        int proxima = revisaoNumero + 1;
+        // É mais intuitivo: "A quilometragem é maior ou igual ao alvo da próxima?"
+        return (quilometragem >= proxima * 10000) || (mesesUso >= proxima * 12);
     }
+    
 
     public void notificarCliente() {
         if (cliente == null || veiculo == null) {
@@ -30,6 +35,7 @@ public class Notificacao {
         if (!gerarAlerta()) {
             return;
         }
+        revisaoNumero++;
         
         System.out.println("ENVIANDO NOTIFICAÇÃO...");
         System.out.println("Cliente: " + cliente.getNome());

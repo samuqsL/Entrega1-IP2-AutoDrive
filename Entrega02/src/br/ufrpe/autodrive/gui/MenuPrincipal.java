2d04cabdafb11686ddc2adf948157fa6924a4b94
samuqsL <samuelcely123@gmail.com>
2026-05-09 14:35:03 -0300

    Add main menu functionality in MenuPrincipal class
    Implemented the main menu for the application with a method to display options and handle user input for sales.

--- Entrega02/src/br/ufrpe/autodrive/gui/MenuPrincipal.java
+++ Entrega02/src/br/ufrpe/autodrive/gui/MenuPrincipal.java
@@ -1,1 +1,23 @@
+package br.ufrpe.autodrive.gui;
 
+import br.ufrpe.autodrive.negocio.IGerenciadorVenda;
+
+public class MenuPrincipal {
+    private IGerenciadorVenda gVenda; // Ele guarda a referência do cérebro do sistema
+
+    public MenuPrincipal(IGerenciadorVenda gVenda) {
+        this.gVenda = gVenda;
+    }
+
+    public void exibirMenu() {
+        // Exemplo:
+        System.out.println("1 - Realizar Venda");
+        // ... lógica de ler opção do usuário ...
+        
+        if (opcao == 1) {
+            // Cria a tela de venda passando o gerenciador que o menu já tem
+            TelaVenda tela = new TelaVenda(this.gVenda);
+            tela.executarBotaoVender();
+        }
+    }
+}

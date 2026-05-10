626cd4d15528f985b30d19867c6ef36c53d09726
samuqsL <samuelcely123@gmail.com>
2026-05-10 15:39:06 -0300

    Update IRepositorioVendas.java

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioVendas.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioVendas.java
@@ -1,5 +1,11 @@
 package br.ufrpe.autodrive.dados;
 
-public interface IRepositorioVendas {
+import br.ufrpe.autodrive.negocio.beans.Venda;
+import java.util.List;
 
+public interface IRepositorioVendas {
+    void adicionarVenda(Venda venda);
+    Venda procurarVenda(String cpf);
+    void removerVenda(); // Conforme seu diagrama
+    List<Venda> listarTodasVendas();
 }

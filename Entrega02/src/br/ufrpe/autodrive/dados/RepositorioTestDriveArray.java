d318f2364d43ef88927a76a04b07e67cefa28021
yuricabral-kkk <yuri.cabral@ufrpe.br>
2026-05-10 17:53:14 -0300

    Update RepositorioTestDriveArray.java

--- Entrega02/src/br/ufrpe/autodrive/dados/RepositorioTestDriveArray.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/RepositorioTestDriveArray.java
@@ -1,5 +1,24 @@
 package br.ufrpe.autodrive.dados;
 
-public class RepositorioTestDriveArray implements IRepositorioTD {
+import br.ufrpe.autodrive.negocio.beans.TestDrive;
+import java.util.ArrayList;
+import java.util.List;
 
+public class RepositorioTestDrive implements IRepositorioTD {
+
+    private List<TestDrive> testDrives;
+
+    public RepositorioTestDrive() {
+        this.testDrives = new ArrayList<>();
+    }
+
+    @Override
+    public void adicionarTestDrive(TestDrive td) {
+        this.testDrives.add(td);
+    }
+
+    @Override
+    public List<TestDrive> listarTestDrives() {
+        return this.testDrives;
+    }
 }

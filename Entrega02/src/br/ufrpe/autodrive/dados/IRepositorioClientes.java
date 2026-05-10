b8a6dcc8b538dbdd4550259a25c43bfdee1debc6
samuqsL <samuelcely123@gmail.com>
2026-05-10 18:30:46 -0300

    Modify procurarCliente to return Cliente object
    Updated the 'procurarCliente' method to return a Cliente object for better integration with the Gerenciador.

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioClientes.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioClientes.java
@@ -1,3 +1,7 @@
+package br.ufrpe.autodrive.dados;
+
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+
 public interface IRepositorioClientes {
     void adicionarCliente(Cliente cliente);
     Cliente procurarCliente(String cpf); // Mudei para retornar Cliente para o Gerenciador usar

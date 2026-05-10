6781cbc3ec340d31a32961f2580d296ce6b0ae7c
samuqsL <samuelcely123@gmail.com>
2026-05-09 21:55:24 -0300

    Change access modifiers of Pessoa class attributes

--- Entrega02/src/br/ufrpe/autodrive/negocio/beans/Pessoa.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/beans/Pessoa.java
@@ -1,50 +1,23 @@
 package br.ufrpe.autodrive.negocio.beans;
 
-/**
- * Classe Abstrata Pessoa
- * Serve como base para Cliente, Vendedor e Mecanico.
- * Por ser abstract, ela não pode ser instanciada diretamente (new Pessoa()).
- */
 public abstract class Pessoa {
-    private String nome;
-    private String cpf;
-    private String telefone;
+    protected String nome;
+    protected String cpf;
+    protected String telefone;
 
-    // Construtor para ser usado pelas classes filhas (super)
+    public Pessoa() {}
+
     public Pessoa(String nome, String cpf, String telefone) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
     }
 
-    // Getters e Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    // Um método toString básico pode ajudar nos relatórios depois
-    @Override
-    public String toString() {
-        return "Nome: " + nome + " | CPF: " + cpf;
-    }
+    // Getters e Setters comuns
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 }

61f01817af4e80fa9cadb900c7ce67052deb3816
samuqsL <samuelcely123@gmail.com>
2026-05-10 15:39:40 -0300

    Update RepositorioVendasArray.java

--- Entrega02/src/br/ufrpe/autodrive/dados/RepositorioVendasArray.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/RepositorioVendasArray.java
@@ -1,5 +1,43 @@
 package br.ufrpe.autodrive.dados;
 
-public class RepositorioVendasArray implements IRepositorioVendas {
+import br.ufrpe.autodrive.negocio.beans.Venda;
+import java.util.ArrayList;
+import java.util.List;
 
+public class RepositorioVendasArray implements IRepositorioVendas {
+    private List<Venda> ListaDeVendas; // Nome exato do seu diagrama
+
+    public RepositorioVendasArray() {
+        this.ListaDeVendas = new ArrayList<>();
+    }
+
+    @Override
+    public void adicionarVenda(Venda venda) {
+        if (venda != null) {
+            this.ListaDeVendas.add(venda);
+        }
+    }
+
+    @Override
+    public Venda procurarVenda(String cpf) {
+        for (Venda v : ListaDeVendas) {
+            if (v.getCliente().getCpf().equals(cpf)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void removerVenda() {
+        // Implementação básica de limpeza ou remoção da última venda
+        if (!ListaDeVendas.isEmpty()) {
+            ListaDeVendas.remove(ListaDeVendas.size() - 1);
+        }
+    }
+
+    @Override
+    public List<Venda> listarTodasVendas() {
+        return new ArrayList<>(ListaDeVendas);
+    }
 }

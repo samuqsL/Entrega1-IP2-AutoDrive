4a035f2b84f78096868b7ebe9c61b782c5307ead
samuqsL <samuelcely123@gmail.com>
2026-05-10 15:19:57 -0300

    Add IRepositorioClientes interface for client management

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioClientes.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioClientes.java
@@ -1,1 +1,5 @@
-
+public interface IRepositorioClientes {
+    void adicionarCliente(Cliente cliente);
+    Cliente procurarCliente(String cpf); // Mudei para retornar Cliente para o Gerenciador usar
+    void removerCliente(Cliente cliente);
+}

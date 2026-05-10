eb081cba9bc7d207ad9c56b0e241fb077dda7e0d
yuricabral-kkk <yuri.cabral@ufrpe.br>
2026-05-10 17:40:09 -0300

    Update GerenciadorTestDrive.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorTestDrive.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorTestDrive.java
@@ -1,5 +1,31 @@
 package br.ufrpe.autodrive.negocio;
 
-public class GerenciadorTestDrive implements IGerenciadorTestDrive {
+import br.ufrpe.autodrive.dados.IRepositorioTD;
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+import br.ufrpe.autodrive.negocio.beans.TestDrive;
+import br.ufrpe.autodrive.negocio.beans.Veiculo;
 
+public class GerenciadorTestDrive {
+    
+    private IRepositorioTD repoTD;
+    private TestDrive testDriveAtual; 
+
+    public GerenciadorTestDrive(IRepositorioTD repoTD) {
+        this.repoTD = repoTD;
+    }
+
+    public boolean agendarTestDrive(Cliente cliente, Veiculo veiculo) {
+        this.testDriveAtual = new TestDrive(cliente, veiculo);
+
+        if (this.testDriveAtual.agendar()) {
+            this.repoTD.adicionarTestDrive(this.testDriveAtual);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public void adicionarTestDrive(TestDrive td) {
+        this.repoTD.adicionarTestDrive(td);
+    }
 }

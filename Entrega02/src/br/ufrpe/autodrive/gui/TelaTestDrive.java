dbebf83a3b354510647deca5716fd41ad3d7e3b9
yuricabral-kkk <yuri.cabral@ufrpe.br>
2026-05-10 17:49:03 -0300

    Update TelaTestDrive.java

--- Entrega02/src/br/ufrpe/autodrive/gui/TelaTestDrive.java
+++ Entrega02/src/br/ufrpe/autodrive/gui/TelaTestDrive.java
@@ -16,11 +16,66 @@
 
 package br.ufrpe.autodrive.gui;
 
+import br.ufrpe.autodrive.negocio.IGerenciadorTestDrive;
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+import br.ufrpe.autodrive.negocio.beans.Veiculo;
+import java.util.Scanner;
+
 public class TelaTestDrive {
 
-  //exibir() é Metodo pra todas as classes! (painelzinho com opções(1, 2, ..., 0[sair]) 
-  //Fiz um na classe "TelaVenda" --- se quiserem uma referência.
-  public void exibir(){
-    
-  }
+    private IGerenciadorTestDrive control;
+
+    public TelaTestDrive(IGerenciadorTestDrive control) {
+        this.control = control;
+    }
+
+    public void exibir() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println("\n--- TELA DE TEST-DRIVE ---");
+            System.out.println("1 - Agendar Test-Drive");
+            System.out.println("0 - Sair (Voltar ao Menu Principal)");
+            System.out.print("Escolha uma opção: ");
+
+            try {
+                opcao = Integer.parseInt(scanner.nextLine());
+
+                switch (opcao) {
+                    case 1:
+                        BotaoAgendarTestDrive(scanner);
+                        break;
+                    case 0:
+                        System.out.println("Retornando ao Menu Principal...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida! Escolha 1 ou 0.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida! Por favor, digite um número.");
+            }
+        }
+    }
+
+    private void BotaoAgendarTestDrive(Scanner scanner) {
+        System.out.println("\n--- AGENDAR TEST-DRIVE ---");
+
+        System.out.print("Digite o CPF do Cliente: ");
+        String cpf = scanner.nextLine();
+
+        System.out.print("Digite o Chassi do Veículo: ");
+        String chassi = scanner.nextLine();
+
+        Cliente clienteSelecionado = new Cliente(); 
+        Veiculo veiculoSelecionado = null; 
+
+        boolean sucesso = this.control.agendarTestDrive(clienteSelecionado, veiculoSelecionado);
+
+        if (sucesso) {
+            System.out.println("Sucesso: Test-drive agendado com sucesso!");
+        } else {
+            System.out.println("Erro: Não foi possível agendar o test-drive. Verifique se o cliente possui CNH válida e se o veículo está disponível.");
+        }
+    }
 }

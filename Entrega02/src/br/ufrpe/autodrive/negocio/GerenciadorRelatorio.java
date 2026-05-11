91ffb0bcd550ffa4f39ab13a851681364a3539b0
OTAVIO-SANTIAGO <santiago888f1@gmail.com>
2026-05-10 22:46:19 -0300

    Update GerenciadorRelatorio.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorRelatorio.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorRelatorio.java
@@ -1,5 +1,28 @@
 package br.ufrpe.autodrive.negocio;
 
-public class GerenciadorRelatorio implements IGerenciadorRelatorio {
+import java.util.List;
+import br.ufrpe.autodrive.negocio.beans.Venda;
+import br.ufrpe.autodrive.negocio.beans.OrdemServico;
+import br.ufrpe.autodrive.dados.IRepositorioVendas;
+import br.ufrpe.autodrive.dados.IRepositorioOS;
 
+public class GerenciadorRelatorio implements IGerenciadorRelatorio {
+    private IRepositorioVendas repoVendas;
+    private IRepositorioOS repoOS;
+
+    public GerenciadorRelatorio(IRepositorioVendas repoVendas, IRepositorioOS repoOS) {
+        this.repoVendas = repoVendas;
+        this.repoOS = repoOS;
+    }
+
+    @Override
+    public List<Venda> gerarRelatorioVendas() {
+        // Retorna a lista pura vinda do repositório
+        return repoVendas.listarTodos();
+    }
+
+    @Override
+    public List<OrdemServico> gerarRelatorioOS() {
+        return repoOS.listarTodas();
+    }
 }

567926252a1dab167c96836bf35576cb83d66b19
Artur Cândido <arturcdn10@gmail.com>
2026-05-13 10:52:50 -0300

    Update IGerenciadorOficina.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
@@ -1,5 +1,18 @@
 package br.ufrpe.autodrive.negocio;
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+import br.ufrpe.autodrive.negocio.beans.MaoDeObra;
+import br.ufrpe.autodrive.negocio.beans.Veiculo;
+import br.ufrpe.autodrive.negocio.beans.Pecas;
 
 public interface IGerenciadorOficina {
+	
+	//abrir OS
+	void abrirOS(int numero, String dataAbertura, Cliente cliente, Veiculo veiculo);
+    
+    // Registrar peças (composição e estoque) e serviços
+    void registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
+    void registrarServicoNaOS(int numeroOS, MaoDeObra servico);
 
+    void finalizarServico(int numeroOS);
+	
 }

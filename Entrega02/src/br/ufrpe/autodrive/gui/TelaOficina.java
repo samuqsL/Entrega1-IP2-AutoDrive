881a4d18121130704d4325ad746aa93c8aa20bc8
Artur Cândido <arturcdn10@gmail.com>
2026-05-13 10:50:28 -0300

    Update TelaOficina.java

--- Entrega02/src/br/ufrpe/autodrive/gui/TelaOficina.java
+++ Entrega02/src/br/ufrpe/autodrive/gui/TelaOficina.java
@@ -1,3 +1,9 @@
+//problema na linha 55, mas não sei qual é o problema
+package br.ufrpe.autodrive.gui;
+import java.util.Scanner;
+import br.ufrpe.autodrive.negocio.IGerenciadorOficina;
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+
 /**
  * ORIENTAÇÃO DE IMPLEMENTAÇÃO (PADRÃO GUI):
  * * 1. MÉTODO exibir(): Deve conter um loop (while) que mantém a tela ativa 
@@ -14,13 +20,53 @@
  * * NOTA: Nenhuma lógica de negócio ou print deve existir fora das classes GUI.
  */
 
-package br.ufrpe.autodrive.gui 
-
 public class TelaOficina {
+	
+	private IGerenciadorOficina control;
 
   //exibir() é Metodo pra todas as classes! (painelzinho com opções(1, 2, ..., 0[sair]) 
   // que chamam metodos especificos das classes do IGerenciciadorX especifico!
   //Fiz um na classe "TelaVenda" --- se quiserem uma referência.
-  public void exibir(){}
+  public void exibir(){
+	  Scanner leitor = new Scanner(System.in);
+	  int op = -1;
+			  
+	  while(op != 0) {
+		  
+		  System.out.println("--------------------------");
+		  System.out.println("  VOCE ENTROU NA OFICINA  ");
+		  System.out.println("--------------------------");
+		  System.out.println("   SELECIONE UMA OPÇÃO    ");
+		  System.out.println("--------------------------");
+		  System.out.println("1. Abrir Ordem de Serviço");
+		  System.out.println("2. Registrar itens da Ordem de Serviço");
+		  System.out.println("0. voltar ao Menu principal");
+		  
+		  switch(op) {
+		  
+		  case(1)://abrir ordem de serviço
+			  System.out.println("Digite o número da nova Ordem de Serviço:");
+		    int numero = leitor.nextInt();
+		    leitor.nextLine(); // Limpar o buffer
+		    
+		    System.out.println("Digite a data (dd/mm/aaaa):");
+		    String data = leitor.nextLine();
+		    
+		    control.abrirOS(numero, data, cliente, veiculo);
+			  break;
+		  case(2):
+			  System.out.println("Digite o número da Ordem de serviço que deseja editar:");
+		    int numOS = leitor.nextInt();
+			  break;
+		  case(0):
+			  System.out.println("voltando ao Menu Principal...");
+			  break;
+		  }
+		  
+	  }
+	  
+  }
+  
+  public void BotaoFinalizarOS() {};
   
 }

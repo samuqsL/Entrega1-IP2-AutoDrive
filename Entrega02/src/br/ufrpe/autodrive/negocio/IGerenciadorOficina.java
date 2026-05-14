b8aaf83ab1da631a9d117024ed57613cf4e47d79
samuqsL <samuelcely123@gmail.com>
2026-05-14 10:57:24 -0300

    Refactor IGerenciadorOficina interface methods
    Updated the abrirOS method to accept CPF and Chassi as parameters instead of Cliente and Veiculo. Modified method signatures for registering parts and services.

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
@@ -1,19 +1,16 @@
 package br.ufrpe.autodrive.negocio;
-import br.ufrpe.autodrive.negocio.beans.*;
-import br.ufrpe.autodrive.negocio.beans.Cliente;
+
 import br.ufrpe.autodrive.negocio.beans.MaoDeObra;
-import br.ufrpe.autodrive.negocio.beans.Veiculo;
 import br.ufrpe.autodrive.negocio.beans.Pecas;
 
 public interface IGerenciadorOficina {
-	
-	//abrir OS
-	boolean abrirOS(int numero, String dataAbertura, Cliente cliente, Veiculo veiculo);
+    // Agora recebe CPF e Chassi para buscar os objetos no repositório
+    boolean abrirOS(int numero, String dataAbertura, String cpfCliente, String chassiVeiculo);
     
-    // Métodos extras exigidos pelo professor (não estão no diagrama, mas são obrigatórios)
-    void registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
-    void registrarServicoNaOS(int numeroOS, MaoDeObra servico);
-
+    // Métodos de registro
+    boolean registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
+    boolean registrarServicoNaOS(int numeroOS, MaoDeObra servico);
+    
+    // Finalização
     boolean finalizarServico(int numeroOS);
-	
 }

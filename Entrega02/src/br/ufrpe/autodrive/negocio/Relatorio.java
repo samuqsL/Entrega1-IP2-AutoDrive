6bc198f5bec751e323673b8d3ea46488389b2892
samuqsL <samuelcely123@gmail.com>
2026-05-14 02:05:49 -0300

    Refactor Relatorio class to use List instead of ArrayList

--- Entrega02/src/br/ufrpe/autodrive/negocio/Relatorio.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/Relatorio.java
@@ -1,94 +1,51 @@
-package br.ufrpe.autodrive.negocio;
-
-import br.ufrpe.autodrive.negocio.beans.*; //Import das classes beans (models)
+package br.ufrpe.autodrive.negocio.beans;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Relatorio {
-    private ArrayList<Venda> listaVendas;
-    private ArrayList<OrdemServico> listaOs;
+    private List<Venda> listaVendas;
+    private List<OrdemServico> listaOs;
 
-    public Relatorio() {
-        this.listaVendas = new ArrayList<>();
-        this.listaOs = new ArrayList<>();
-    }
-  
-public Relatorio(ArrayList<Venda> listaVendas, OrdemServico os) {
-    this.listaVendas = (listaVendas != null) ? listaVendas : new ArrayList<>();
-    this.listaOs = new ArrayList<>();
-    if (os != null) {
-        this.listaOs.add(os);
-    }
-}
-
-
-    public void adicionarVenda(Venda venda) {
-        this.listaVendas.add(venda);
+    public Relatorio(List<Venda> vendas, List<OrdemServico> ordens) {
+        this.listaVendas = (vendas != null) ? new ArrayList<>(vendas) : new ArrayList<>();
+        this.listaOs = (ordens != null) ? new ArrayList<>(ordens) : new ArrayList<>();
     }
 
-    public void adicionarOS(OrdemServico os) {
-        this.listaOs.add(os);
-    }
-
-    public void relatorioVendas() {
-        System.out.println("--- Relatório Geral de Vendas ---");
+    // Filtros para o REQ09
+    public List<Venda> filtrarPorVendedor(String nomeVendedor) {
+        List<Venda> filtradas = new ArrayList<>();
         for (Venda v : listaVendas) {
-            System.out.println("Venda: R$ " + v.getValorTotal() + " | Data: " + v.getDataVenda());
-        }
-    }
-
-    public void vendasVendedor(Vendedor vendedor) {
-        System.out.println("--- Vendas do Vendedor: " + vendedor.getNome() + " ---");
-        for (Venda v : listaVendas) {
-            if (v.getVendedor().getNome().equals(vendedor.getNome())) {
-                System.out.println("Data: " + v.getDataVenda() + " | Valor: R$ " + v.getValorTotal());
+            if (v.getVendedor().getNome().equalsIgnoreCase(nomeVendedor)) {
+                filtradas.add(v);
             }
         }
+        return filtradas;
     }
 
-    public void vendasPeriodo(String inicio, String fim) {
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dataIni = LocalDate.parse(inicio, fmt);
-        LocalDate dataFim = LocalDate.parse(fim, fmt);
-
-        System.out.println("--- Vendas de " + inicio + " a " + fim + " ---");
+    public List<Venda> filtrarPorPeriodo(LocalDate inicio, LocalDate fim) {
+        List<Venda> filtradas = new ArrayList<>();
         for (Venda v : listaVendas) {
-            if (v.getDataVenda() != null) {
-                LocalDate dtVenda = v.getDataVenda().toLocalDate();
-                if (!(dtVenda.isBefore(dataIni) || dtVenda.isAfter(dataFim))) {
-                    System.out.println("Data: " + dtVenda + " | Valor: R$ " + v.getValorTotal());
-                }
+            LocalDate dataVenda = v.getDataVenda().toLocalDate();
+            if (!(dataVenda.isBefore(inicio) || dataVenda.isAfter(fim))) {
+                filtradas.add(v);
             }
         }
+        return filtradas;
     }
 
-    public void emitirRelatorioOS() {
-        System.out.println("--- Relatório Geral da Oficina ---");
+    // Cálculos para o REQ11
+    public double[] calcularLucratividade() {
+        double pecas = 0;
+        double servicos = 0;
         for (OrdemServico os : listaOs) {
-            System.out.println("OS Nº: " + os.getNumero() + " | Status: " + os.getStatus());
+            for (Pecas p : os.getListaPecas()) pecas += p.custoPecas();
+            for (MaoDeObra m : os.getListaServicos()) servicos += m.calcularCusto();
         }
+        return new double[]{pecas, servicos};
     }
-    
-    public void relatorioLucratividadeOficina() {
-       double lucroPecas = 0;
-       double totalServicos = 0;
 
-       for (OrdemServico os : listaOs) {
-           for (Pecas p : os.getListaPecas()) {
-               lucroPecas += p.custoPecas();
-           }
-           
-           for (MaoDeObra m : os.getListaServicos()) {
-               totalServicos += m.calcularCusto();
-            }
-       }
-        
-       System.out.println("\n--- RELATÓRIO DE LUCRATIVIDADE DA OFICINA ---");
-       System.out.println("Receita com Peças: R$ " + String.format("%.2f", lucroPecas));
-       System.out.println("Receita com Serviços: R$ " + String.format("%.2f", totalServicos));
-       System.out.println("Total da Oficina: R$ " + String.format("%.2f", (lucroPecas + totalServicos)));
-       System.out.println("----------------------------------------------");
-    }
+    public List<Venda> getListaVendas() { return new ArrayList<>(listaVendas); }
+    public List<OrdemServico> getListaOs() { return new ArrayList<>(listaOs); }
 }
